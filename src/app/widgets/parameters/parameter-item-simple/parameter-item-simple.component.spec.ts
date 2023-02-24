d8ff681e93d8fabf9fc2380f201c13bb21347f05
Clemens Prescher <clemens.prescher@gmail.com>
2023-02-24 12:00:33 +0100

    First tests using angular materials harness

--- src/app/widgets/parameters/parameter-item-simple/parameter-item-simple.component.spec.ts
+++ src/app/widgets/parameters/parameter-item-simple/parameter-item-simple.component.spec.ts
@@ -2,10 +2,17 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ParameterItemSimpleComponent} from './parameter-item-simple.component';
 import {MaterialsModule} from "../../../shared/gui/materials.module";
+import {Parameter} from "../../../shared/parameter.model";
+import {HarnessLoader} from "@angular/cdk/testing";
+import {TestbedHarnessEnvironment} from "@angular/cdk/testing/testbed";
+import {MatInputHarness} from "@angular/material/input/testing";
+import {MatFormFieldHarness} from "@angular/material/form-field/testing";
 
 describe('ParameterItemSimpleComponent', () => {
   let component: ParameterItemSimpleComponent;
   let fixture: ComponentFixture<ParameterItemSimpleComponent>;
+
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +23,26 @@ describe('ParameterItemSimpleComponent', () => {
 
     fixture = TestBed.createComponent(ParameterItemSimpleComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Shows the correct parameter name', async () => {
+    component.parameter = new Parameter("Pixel value", 23);
+
+    const parameterFormField = await loader.getHarness<MatFormFieldHarness>(MatFormFieldHarness);
+    expect(await parameterFormField.getLabel()).toEqual("Pixel value:");
   });
+
+  it("Emits the correct parameter value", async () => {
+    component.parameter = new Parameter("Pixel value", 23);
+
+    const parameterInput = await loader.getHarness<MatInputHarness>(MatInputHarness);
+    await parameterInput.setValue("53");
+
+    const host = await parameterInput.host();
+    await host.dispatchEvent("change");
+
+    expect(Number(component.parameter.value)).toEqual(53);
+  })
 });
