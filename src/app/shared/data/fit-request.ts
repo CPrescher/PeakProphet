1eccae71bd1f02281708d24f96b50a71af869863
Clemens Prescher <clemens.prescher@gmail.com>
2025-02-14 20:51:00 +0100

    Added environments to handle different backend urls

--- src/app/shared/data/fit-request.ts
+++ src/app/shared/data/fit-request.ts
@@ -1,4 +1,4 @@
-import {FitModel} from "./fit-model";
+import { FitModel } from './fit-model';
 import {
   distinctUntilChanged,
   filter,
@@ -6,15 +6,15 @@ import {
   interval,
   Observable,
   ReplaySubject,
-  Subject, Subscription,
+  Subject,
+  Subscription,
   take,
-  takeUntil
-} from "rxjs";
-import {io, Socket} from "socket.io-client";
-import {isDevMode} from "@angular/core";
+  takeUntil,
+} from 'rxjs';
+import { io, Socket } from 'socket.io-client';
+import { environment } from '../../../environments/environment';
 
 export class FitRequest {
-
   private resultSubject: Subject<any> = new Subject<any>();
   public result$: Observable<any>;
 
@@ -40,14 +40,14 @@ export class FitRequest {
   }
 
   public fit(): [Observable<any>, Observable<any>, Subject<void>] {
-    const json_data = this._createJSONData(this.fitModel)
+    const json_data = this._createJSONData(this.fitModel);
 
     this._connectToSocketIO();
     this._emitFitRequest(json_data);
     this._emitProgressRequest();
     this._createStopper();
 
-    return [this.result$, this.progress$, this.stopper$]
+    return [this.result$, this.progress$, this.stopper$];
   }
 
   private _createJSONData(fitModel: FitModel) {
@@ -55,27 +55,22 @@ export class FitRequest {
       name: fitModel.name,
       pattern: fitModel.pattern,
       peaks: fitModel.peaks,
-      background: fitModel.background
-    })
+      background: fitModel.background,
+    });
   }
 
   private _connectToSocketIO() {
-    if (isDevMode()) {
-      this.sioClient = io('http://localhost:8009');
-    } else {
-      this.sioClient = io('https://peakprophet.com:8009');
-    }
+    this.sioClient = io(environment.backend_url);
 
-    this.sioConnect$ = fromEvent(this.sioClient, 'connect')
+    this.sioConnect$ = fromEvent(this.sioClient, 'connect');
     this.sioDisconnect$ = fromEvent(this.sioClient, 'disconnect').pipe(take(1));
   }
 
   private _emitFitRequest(json_data: string) {
     this.resultSubject = new Subject<any>();
-    this.result$ = this.resultSubject.asObservable().pipe(
-      takeUntil(this.sioDisconnect$),
-      take(1),
-    )
+    this.result$ = this.resultSubject
+      .asObservable()
+      .pipe(takeUntil(this.sioDisconnect$), take(1));
 
     this._connectSubscription = this.sioConnect$.subscribe(() => {
       this.sioClient.emit('fit', json_data, (payload) => {
@@ -87,14 +82,13 @@ export class FitRequest {
       this._firstProgress = true;
     });
 
-
     this._resultSubscription = this.result$.subscribe({
       complete: () => {
         setTimeout(() => {
           this.sioClient.disconnect();
           this.sioClient.close();
         }, 500);
-      }
+      },
     });
 
     this._disconnectSubscription = this.sioDisconnect$.subscribe(() => {
@@ -104,28 +98,30 @@ export class FitRequest {
 
   private _emitProgressRequest() {
     this.progressSubject.complete();
-    this.progressSubject = new ReplaySubject<any>(10)
+    this.progressSubject = new ReplaySubject<any>(10);
 
     this.progress$ = this.progressSubject.asObservable().pipe(
-      filter(payload => payload !== undefined),
+      filter((payload) => payload !== undefined),
       distinctUntilChanged((prev, current) => prev.iter === current.iter),
       takeUntil(this.sioDisconnect$),
-      takeUntil(this.result$),
-    )
+      takeUntil(this.result$)
+    );
 
     this._progressRequestSubscription = this.sioConnect$.subscribe(() => {
-      this._progressIntervalSubscription = interval(30).pipe(
-        filter(() => this._requestProgress || this._firstProgress),
-        takeUntil(this.sioDisconnect$),
-        takeUntil(this.result$),
-      ).subscribe(() => {
-        this.sioClient.emit('request_progress', (payload) => {
-          this.progressSubject.next(payload);
-          this._requestProgress = true;
-          this._firstProgress = false;
+      this._progressIntervalSubscription = interval(30)
+        .pipe(
+          filter(() => this._requestProgress || this._firstProgress),
+          takeUntil(this.sioDisconnect$),
+          takeUntil(this.result$)
+        )
+        .subscribe(() => {
+          this.sioClient.emit('request_progress', (payload) => {
+            this.progressSubject.next(payload);
+            this._requestProgress = true;
+            this._firstProgress = false;
+          });
+          this._requestProgress = false;
         });
-        this._requestProgress = false;
-      });
     });
   }
 
@@ -134,7 +130,7 @@ export class FitRequest {
     this.stopper$ = new Subject<void>();
     this.stopper$.subscribe(() => {
       this.sioClient.emit('stop');
-      this.sioClient.disconnect()
+      this.sioClient.disconnect();
       this.sioClient.close();
       this._requestProgress = true;
     });
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  backend_url: 'https://peak-prophet-server-1017460659536.europe-west1.run.app'
+};
